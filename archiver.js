c440e2996e247ea1834bdb5ee5bc7b9b1fb16c6a
Michael Stewart <tgm@vt.edu>
2015-02-19 09:03:58 -0500

    working on getting max_id and since_id query working

--- archiver.js
+++ archiver.js
@@ -1,20 +1,23 @@
 var Twit = require('twit');
 var CronJob = require('cron').CronJob;
+var RSVP = require('rsvp');
 
 var T = new Twit({
-    consumer_key:         'dqtsMI0WiSBAoQYrSf1ygor0Y'
-  , consumer_secret:      'pP4jsDtZVPZiGXK4le5hTqzTmKccuTFLKWmlPGwaE0AdsBmA2D'
-  , access_token:         '15250294-0bYySsaSF5fd9WsWFuvs8iwPcZpzIOCNcj6WOZD4u'
-  , access_token_secret:  'UO7ONwgMLhGeRylc45sZw060lXK7RpAygI9hd7oplivAp'
+  consumer_key:         'dqtsMI0WiSBAoQYrSf1ygor0Y',
+  consumer_secret:      'pP4jsDtZVPZiGXK4le5hTqzTmKccuTFLKWmlPGwaE0AdsBmA2D',
+  access_token:         '15250294-0bYySsaSF5fd9WsWFuvs8iwPcZpzIOCNcj6WOZD4u',
+  access_token_secret:  'UO7ONwgMLhGeRylc45sZw060lXK7RpAygI9hd7oplivAp'
 });
 
-const QUERY = "#gpptripviz123";
-const COUNT = 100;
+var QUERY = "#gpptripviz123";
+var COUNT = 100;
 
 var Sequelize = require('sequelize')
-  , sequelize = new Sequelize('gpptripviz', 'gpptripviz', 'gpptripviz')
+  , sequelize = new Sequelize('gpptripviz', 'gpptripviz', 'gpptripviz', {
+      logging: function(){}
+    });
  
-var Tweet = sequelize.define('Tweet', {
+var Tweet = sequelize.define('tweet', {
   twitter_id: Sequelize.STRING,
   text: Sequelize.STRING,
   lat: Sequelize.FLOAT,
@@ -22,19 +25,19 @@ var Tweet = sequelize.define('Tweet', {
   dateTime: Sequelize.DATE
 });
 
-var Media = sequelize.define('Media', {
+var Media = sequelize.define('media', {
   twitter_id: Sequelize.STRING,
   url: Sequelize.STRING
 });
 
-var User = sequelize.define('User', {
+var User = sequelize.define('user', {
   screenName: Sequelize.STRING,
   name: Sequelize.STRING,
   image: Sequelize.STRING,
   twitter_id: Sequelize.STRING
 });
 
-var Run = sequelize.define('Run', {
+var Run = sequelize.define('run', {
   start_time: Sequelize.DATE,
   end_time: Sequelize.DATE,
   max_id: Sequelize.STRING,
@@ -47,10 +50,13 @@ Media.belongsTo(User);  // a user may have many media
 Media.belongsTo(Run);   // a run may have many media
 Tweet.belongsTo(Run);   // a run may have many tweets
 User.belongsTo(Run);    // a run may have many users
- 
-sequelize.sync().success(function() {
-  console.log('success!');
-});
+
+var dbReady = RSVP.all([
+  Tweet.sync(),
+  Media.sync(),
+  User.sync(),
+  Run.sync()
+]);
 
 var accumulator = [];
 
@@ -71,7 +77,7 @@ function gotTweets (err, data, response, startTime) {
     }).success(function(runObj){
 
       if (data.hasOwnProperty('statuses')) {
-      //console.log('gotTweets has statuses');
+      console.log('gotTweets has statuses', data.statuses);
 
       accumulator.push.apply(accumulator, data.statuses);
 
@@ -81,7 +87,7 @@ function gotTweets (err, data, response, startTime) {
       if (data.hasOwnProperty('search_metadata') && 
         data.search_metadata.hasOwnProperty('next_results') && 
         data.next_results.length>0) {
-        console.log('next results');
+        // console.log('next results');
         // ?max_id=475034902396960767&q=%23gpp14%20since%3A2014-05-27&include_entities=1
         var maxIdPos = data.next_results.indexOf('max_id=');
         var endMaxPos = data.next_results.indexOf('&', max_id);
@@ -89,7 +95,7 @@ function gotTweets (err, data, response, startTime) {
         T.get('search/tweets', { q: query, max_id: maxId}, gotTweets); 
       }
       else {
-        console.log('gotTweets has NO MORE statuses');
+        // console.log('gotTweets has NO MORE statuses');
         var tweetsToArchive = withoutRetweetsAndUnlocated(accumulator).statuses;
         //console.log(tweetsToArchive);
 
@@ -98,7 +104,7 @@ function gotTweets (err, data, response, startTime) {
           var theCurrentTweet = tweetsToArchive[i];
           
           if (theCurrentTweet.hasOwnProperty('user')) {
-            console.log('current tweet has user property');
+            // console.log('current tweet has user property');
             User.findOrCreate({ where: {twitter_id: theCurrentTweet.user.id_str }})
               .spread(function(user, created) {
                 if (created) {
@@ -129,8 +135,8 @@ function gotTweets (err, data, response, startTime) {
               .then(function(t) {
                 // console.log(t);
                 if (theCurrentTweet.hasOwnProperty('entities') && theCurrentTweet.entities.hasOwnProperty('media')) {
-                    console.log("theCurrentTweet.entities.media");
-                    console.log(theCurrentTweet.entities.media[0]);
+                    // console.log("theCurrentTweet.entities.media");
+                    // console.log(theCurrentTweet.entities.media[0]);
                   for (var j=0; j<theCurrentTweet.entities.media.length; j++) {
                     var theCurrentMedium = theCurrentTweet.entities.media[j];
                     var m = Media.create({
@@ -138,7 +144,7 @@ function gotTweets (err, data, response, startTime) {
                       url: theCurrentMedium.media_url
                     })
                       .then(function(m){
-                        console.log("check promise");
+                        // console.log("check promise");
                         t.getUser()
                           .then(function (userObject) {
                             m.setUser(userObject);
@@ -158,14 +164,24 @@ function gotTweets (err, data, response, startTime) {
   }
 }
 
-var runStartTime = new Date();
-// TODO: instead of just getting whatever tweets, we shoudl add since_id to the arguments here, 
-// but where will we get it from? (the max id of the run)
-// we shoudl find the most recent Run in the db, mabe by the one 
-// with maximum max_id or by maximum endTime or something
-T.get('search/tweets', { q: QUERY, count: COUNT }, function(e,d,r){
-  gotTweets(e, d, r, runStartTime);
-});
+dbReady
+  .then(function() {
+    Run.max('max_id')
+      .then(function(maxId) {
+        var runStartTime = new Date();
+        // TODO: instead of just getting whatever tweets, we shoudl add since_id to the arguments here, 
+        // but where will we get it from? (the max id of the run)
+        // we shoudl find the most recent Run in the db, mabe by the one 
+        // with maximum max_id or by maximum endTime or something
+        T.get('search/tweets', { q: QUERY, count: COUNT, since_id: maxId}, function(e,d,r){
+          gotTweets(e, d, r, runStartTime);
+        });
+      });
+  })
+  .catch(function(error){
+    console.error("error syncing db", error);
+  });
+  
 
 function withoutRetweetsAndUnlocated (statuses) {
   //console.log(statuses);
@@ -173,11 +189,11 @@ function withoutRetweetsAndUnlocated (statuses) {
   for (var i=0; i<statuses.length; i++) {
      //console.log(i);
     if (statuses[i].geo!=null && !(statuses[i].hasOwnProperty('retweeted_status'))) {
-      console.log('if');
+      // console.log('if');
       results.push(statuses[i]);
     }
     else {
-      console.log('else');
+      // console.log('else');
       //console.log(statuses[i]);
     }
   }
