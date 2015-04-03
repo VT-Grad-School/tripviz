9b1fe04eee232fdad67e750cd7976715ade513d9
James Matthew Beamer <jmb2331@vt.edu>
2015-04-03 15:56:47 -0400

    Populated tweet feed.

--- app/scripts/controllers/tweet.js
+++ app/scripts/controllers/tweet.js
@@ -1,13 +1,23 @@
 'use strict';
 
 angular.module('tripvizApp')
-  .controller('TweetCtrl', function ($scope, $http, Tweets, Locations) {
+  .controller('TweetCtrl', function ($scope, $http, Tweets, Locations, $stateParams) {
+  	console.log($stateParams);
     $scope.tweetService = Tweets;
     $scope.locations = [];
     $scope.locationsService = Locations;
+    $scope.city = 'Blacksburg';
+    $scope.city = $stateParams.location;
     Locations.getLocations().then(function (locations) {
       $scope.locations = locations;
     });
+
+    $scope.feed = [];
+
+    Tweets.getTweetsForLoc($scope.city)
+    	.then(function (filteredTweets) {
+    		$scope.feed = filteredTweets;
+    	});
 
     // console.log($scope.locations);
   });
